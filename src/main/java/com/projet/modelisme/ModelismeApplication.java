6c7bca7c9bdc37e9c801eaabbb54025eb3475fc7
unknown <geoffreylaunay02@gmail.com>
2024-08-16 21:38:50 +0200

    testcommit

---
+++ src/main/java/com/projet/modelisme/ModelismeApplication.java
@@ -0,0 +1,13 @@
+package com.projet.modelisme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelismeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelismeApplication.class, args);
+	}
+
+}
